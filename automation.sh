35794158baa4a4e17b05ca79b233842434d21851
Prakash Behera <kprakashbehera@gmail.com>
2023-03-01 17:04:21 +0000

    v2
    version 2 with file upload command

--- automation.sh
+++ automation.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+myname="Prakash"
+s3_bucket="upgrad-prakashkumar"
+
+#Installing apache webserver
 sudo apt update -y
 sudo apt install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
+
+#Creating tar in tmp folder
+sudo tar -cvf /tmp/Prakash-httpd-logs-$(date '+%d%m%Y-%H%M%S').log /var/log/apache2
+
+#Copying files to s3 bucket
+sudo aws s3 cp /tmp/${myname}-httpd-logs-*.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
